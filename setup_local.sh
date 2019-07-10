00f52e980cec53f7b374a657fa589cc449986219
Michael Conard <maconard@mtu.edu>
2019-07-10 11:28:55 -0500

    fix copy paste error on script

--- setup_local.sh
+++ setup_local.sh
@@ -5,7 +5,7 @@ echo -e "{
   \"cloud_object_storage_apikey\": \"$key\",
   \"cloud_object_storage_endpoints\": \"\",
   \"cloud_object_storage_iam_apikey_description\": \"API key for hackathon project\",
-  \"cloud_object_iam_apikey_name\": \"API key for hackathon project\",
+  \"cloud_object_iam_apikey_name\": \"watson-code-reviewer-key\",
   \"cloud_object_storage_iam_serviceid_crn\": \"\",
   \"cloud_object_storage_resource_instance_id\": \"\"
 }" > server/localdev-config.json
