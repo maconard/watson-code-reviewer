b25a5887d5d993da1986ae79bb57869bffaad6cb
Michael Conard <maconard@mtu.edu>
2019-07-11 12:01:07 -0500

    new js files

---
+++ public/js/analysis.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    let submitCodeBtn = $('#watson-submit-button');
+    let resetCodeBtn = $('#watson-reset-button');
+    let codeBox = $('#codebox');
+    let formatBox = $('#formatted-code');
+
+    codeBox.on('keyup', function(e) {
+        var code = codeBox.val();
+        if(code.length == 0) {
+            submitCodeBtn.attr('disabled',true);
+        } else {
+            submitCodeBtn.attr('disabled',false);
+        }
+    });
+
+    submitCodeBtn.on('click', function(e) {
+        var code = codeBox.val();
+        //console.log("Submitted code:");
+        //console.log(code);
+
+        console.log(genCodeStatistics(code)); 
+
+        codeBox.hide();
+        formatBox.html("<pre>" + code + "</pre>");
+        formatBox.parent().show();
+        resetCodeBtn.attr('disabled',false);
+        submitCodeBtn.attr('disabled',false);
+    });
+
+    resetCodeBtn.on('click', function(e) {
+        codeBox.val("");
+        formatBox.html("");
+        resetCodeBtn.attr('disabled',true);
+        formatBox.parent().hide();
+        codeBox.show();
+    });
+});
