508d4250ed5f2536ad1d6aea61a1b1861e2bb5e6
Michael Conard <maconard@mtu.edu>
2019-07-11 11:35:36 -0500

    watson payload processing

--- server/process.js
+++ server/process.js
@@ -3,7 +3,7 @@ const request = require("request");
 const params = require("./watsonmodelparams");
 const wml_credentials = new Map();
 
-const mlInstanceId = params.mlInstanceId;
+const mlInstanceID = params.mlInstanceId;
 const apikey = params.apikey;
 const scoring_url = params.scoring_url;
 const options = params.options;
@@ -30,7 +30,8 @@ function apiPost(payload, loadCallback, errorCallback){
 }
 
 function sendPayload(dataArray) {
-    const payload = "";
+    const payload = '{"fields": ["maxLineLength", "avgLineLength", "avgParensPerLine", "maxParensPerLine", "avgParenSpaceBuffersPerLine", "avgPeriodsPerLine", "maxPeriodsPerLine", "avgComparisonsPerLine", "maxComparisonsPerLine", "avgSpacesPerLine", "maxSpacesPerLine", "avgTabsPerLine", "maxTabsPerLine", "avgIdentifiersPerLine", "maxIdentifiersPerLine"], "values": [[' + dataArray + ']]}';
+    //console.log(payload);
 
     apiPost(payload, function (resp) {
 	    let parsedPostResponse;
@@ -39,8 +40,12 @@ function sendPayload(dataArray) {
 	    } catch (ex) {
 		    // TODO: handle parsing exception
 	    }
-	    console.log("Scoring response:");
-	    console.log(parsedPostResponse);
+	    //console.log("Scoring response:");
+	    //console.log(parsedPostResponse);
+        
+        var res = parsedPostResponse.values[0];
+        //console.log("probability: " + res[17]);
+        console.log("\nScore: " + (res[17][0] >= .70 ? "Readable, " : "Not readable, ") + Math.round(res[17][0] * 1000,0.1)/10.0 + " points");
     }, function (error) {
         console.log("Scoring error:");
 	    console.log(error);
@@ -48,13 +53,16 @@ function sendPayload(dataArray) {
 }
 
 function testPayloads() {
-    //sendPayload(p1);
+    //console.log("Sending test payload...");
+
+    //sendPayload([62,29.85714286,1.428571429,4,0,0.714285714,2,0.428571429,2,1.714285714,5,1.857142857,3,3.285714286,6]);
+    //sendPayload([171,35.13333333333333,1.7333333333333334,8,0.06666666666666667,0.8,7,0.2,3,3,18,1.1333333333333333,2,4.466666666666667,26]);
 }
 
 request.post(options, function(error, response, body)
 {
     iamToken = JSON.parse(body)["access_token"];
     wmlToken = "Bearer " + iamToken;
-    //testPayloads();
+    testPayloads();
 });
 
--- server/watsonmodelparams.js
+++ server/watsonmodelparams.js
@@ -5,7 +5,7 @@ btoa = require("btoa");
 
 module.exports = {
     mlInstanceId: "df542fb4-f3c6-47a6-8b8a-3e0632efb43c",
-    scoring_url: "",
+    scoring_url: "https://us-south.ml.cloud.ibm.com/v3/wml_instances/df542fb4-f3c6-47a6-8b8a-3e0632efb43c/deployments/d3c99ab0-252b-45e2-9e90-9d16e9450c85/online",
     apikey: apikey,
     options: { 
         url     : "https://iam.bluemix.net/oidc/token",
